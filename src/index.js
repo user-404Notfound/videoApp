57b9f88333675290a9595795cb99fe70d60cd0de
user-404Notfound <sanmesh2007@gmail.com>
2026-08-02 14:27:50 +0000

    added express app and .env example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './db/db.js';
+import app from './app.js'
 
 dotenv.config({
     path:"../.env"
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+
+    const port = process.env.PORT || 8000;
+
+    app.listen(port,()=>{
+        console.log(`serving at http://localhost:${port}`)
+    })
+
+}).catch((err0r)=>{
+    console.log("MongoDB connection failed: ",error)
+})
