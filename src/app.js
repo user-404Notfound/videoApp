f35271dcba56746842daba6aab7ad8c2a4b91164
user-404Notfound <sanmesh2007@gmail.com>
2026-08-09 09:41:26 +0000

    added custom ApiError and ApiResponse

--- src/app.js
+++ src/app.js
@@ -14,4 +14,8 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+app.get("/",(req,res) => {
+    res.send("server is running");
+})
+
 export default app;
