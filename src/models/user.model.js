804559947c4d25f9d64162d942cfd1083d917747
user-404Notfound <sanmesh2007@gmail.com>
2026-08-11 01:39:48 +0000

    add access and refresh token generation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose, {Schema} from 'mongoose';
 import bcrypt from 'bcrypt';
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
         username:{
             type:String,
             required:true,
@@ -51,7 +51,7 @@ const UserSchema = new Schema({
     }
 )
 
-UserSchema.pre("save", async function (next){
+userSchema.pre("save", async function (next){
         if (!this.isModified("password")) return next();
 
         this.password = await bcrypt.hash(this.password,10);
@@ -60,8 +60,34 @@ UserSchema.pre("save", async function (next){
     }
 )
 
-UserSchema.methods.isPasswordCorrect = async function (password){
+userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password,this.password)
 }
 
-export const User = mongoose.model("User",UserSchema)
+userSchema.methods.generateAccessToken =  function (){
+    return  jwt.sign(
+        {
+            _id:this._id,
+            username:this.username,
+            email:this.email,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken =  function (){
+    return  jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+export const User = mongoose.model("User",userSchema)
